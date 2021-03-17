5262c68555a0be6120e1b9171d222566d85e2918
Doug Schwartz <dougsch@amazon.com>
2021-03-17 06:02:04 -0700

    Renaming some directories

---
+++ ALPHA/tmp/DynamoDB/ListTables/src/main.rs
@@ -0,0 +1,50 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+use clap::{App, Arg};
+use std::error::Error;
+
+use dynamodb::operation::ListTables;
+use dynamodb::Region; // dynamodb::{Credentials, Endpoint, Region};
+use env_logger::Env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let matches = App::new("myapp")
+        .arg(
+            Arg::with_name("region")
+                .short("r")
+                .long("region")
+                .value_name("REGION")
+                .help("Specifies the region to create the table in")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let region = matches.value_of("region").unwrap_or("us-west-2");
+    println!("Value for region: {}", region);
+
+    println!("DynamoDB client version: {}", dynamodb::PKG_VERSION);
+    let config = dynamodb::Config::builder()
+        .region(Region::from(region))
+        .build();
+    let client = aws_hyper::Client::https();
+
+    let op = ListTables::builder().build(&config);
+
+    let tables = client.call(op).await?;
+
+    println!("\nTables in {}:\n", region);
+
+    for name in tables.table_names.iter() {
+        for n in name.iter() {
+            println!("    {:?}", n);
+        }
+    }
+
+    Ok(())
+}
