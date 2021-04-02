d45b9f5c51bbc3f4db3197393b85a1f98f0ec564
Doug Schwartz <dougsch@amazon.com>
2021-04-02 05:44:01 -0700

    Finished first pass at KMS Encrypt and Decrypt code examples in Rust

--- ALPHA/kms/Decrypt/src/main.rs
+++ ALPHA/kms/Decrypt/src/main.rs
@@ -45,20 +45,15 @@ async fn main() {
         .get_matches();
 
     let region = matches.value_of("region").unwrap_or("us-west-2");
-    let key = matches
-        .value_of("key")
-        .unwrap_or("0249d461-754b-4cbd-b874-372b294fa354");
+    let key = matches.value_of("key").unwrap_or("");
     let text = matches.value_of("text").unwrap_or("");
 
     if region == "" || key == "" || text == "" {
-        println!("You must supply a value for region, key, and file containing encrypted text (-r REGION -k KEY -t TEXT-FILE)");
+        println!("You must supply a value for region, key, and file containing encrypted text ([-r REGION] -k KEY -t TEXT-FILE)");
+        println!("If REGION is omitted, defaults to us-west-2");
 
         process::exit(1);
     }
-
-    println!("Region:    {}", region);
-    println!("Key:       {}", key);
-    println!("Text file: {}", text);
 
     SubscriberBuilder::default()
         .with_env_filter("info")
@@ -70,8 +65,6 @@ async fn main() {
 
     // Vector to hold the string parts/u8 values
     let mut v_bytes: Vec<u8> = Vec::new();
-    let mut i = 0;
-    //let mut num: u8;
 
     // Open text file and get contents as a string
     match fs::read_to_string(text) {
@@ -79,25 +72,11 @@ async fn main() {
             // Split the string into parts by comma
             let parts = input.split(",");
             for s in parts {
-                // Each part should be a u8
-                // Print out the string so we can tell if it's a legit u8
                 // Trim any trailing line feed
                 let mut my_string = String::from(s);
-                /*
-                if my_string.ends_with('\n') {
-                    println!("Dropping \\n from string");
-                    my_string.pop();
-                    if my_string.ends_with('\r') {
-                        println!("Dropping \\r from string");
-                        my_string.pop();
-                    }
-                }
-                */
 
                 let len = my_string.trim_end_matches(&['\r', '\n'][..]).len();
                 my_string.truncate(len);
-
-                println!("String at {}: '{}'", i, my_string);
 
                 match my_string.parse::<u8>() {
                     Ok(num) => v_bytes.push(num),
@@ -106,36 +85,11 @@ async fn main() {
                         panic!("{}", e)
                     }
                 }
-                i = i + 1;
             }
         }
         Err(_) => println!("Could not parse {} as a string", text),
     }
 
-    // Format of the file:
-    //   BYTE1, ..., BYTEN
-
-    //let mut buf = text.as_bytes();
-
-    /*
-        if text == "" {
-            buf = &[
-                1u8, 2u8, 2u8, 0u8, 120u8, 45u8, 38u8, 92u8, 185u8, 80u8, 21u8, 159u8, 165u8, 76u8,
-                198u8, 157u8, 254u8, 232u8, 51u8, 65u8, 96u8, 186u8, 181u8, 89u8, 108u8, 127u8, 9u8,
-                91u8, 193u8, 213u8, 185u8, 211u8, 65u8, 89u8, 139u8, 47u8, 89u8, 1u8, 121u8, 251u8,
-                127u8, 140u8, 106u8, 72u8, 73u8, 77u8, 86u8, 149u8, 106u8, 180u8, 51u8, 247u8, 242u8,
-                203u8, 0u8, 0u8, 0u8, 115u8, 48u8, 113u8, 6u8, 9u8, 42u8, 134u8, 72u8, 134u8, 247u8,
-                13u8, 1u8, 7u8, 6u8, 160u8, 100u8, 48u8, 98u8, 2u8, 1u8, 0u8, 48u8, 93u8, 6u8, 9u8,
-                42u8, 134u8, 72u8, 134u8, 247u8, 13u8, 1u8, 7u8, 1u8, 48u8, 30u8, 6u8, 9u8, 96u8,
-                134u8, 72u8, 1u8, 101u8, 3u8, 4u8, 1u8, 46u8, 48u8, 17u8, 4u8, 12u8, 127u8, 158u8,
-                249u8, 221u8, 238u8, 76u8, 50u8, 186u8, 19u8, 227u8, 217u8, 171u8, 2u8, 1u8, 16u8,
-                128u8, 48u8, 231u8, 36u8, 43u8, 64u8, 19u8, 78u8, 101u8, 237u8, 115u8, 30u8, 241u8,
-                85u8, 102u8, 164u8, 168u8, 199u8, 193u8, 102u8, 118u8, 138u8, 27u8, 8u8, 194u8, 100u8,
-                85u8, 81u8, 160u8, 18u8, 78u8, 23u8, 33u8, 27u8, 12u8, 84u8, 209u8, 165u8, 100u8, 31u8,
-                194u8, 179u8, 197u8, 122u8, 182u8, 183u8, 95u8, 247u8, 219u8, 162u8,
-            ];
-        }
-    */
     let blob = Blob::new(v_bytes);
 
     let resp = client
@@ -157,5 +111,7 @@ async fn main() {
         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
     };
 
-    println!("String: {}", s)
+    println!("");
+    println!("Decoded string:");
+    println!("{}", s);
 }
--- ALPHA/kms/Encrypt/src/main.rs
+++ ALPHA/kms/Encrypt/src/main.rs
@@ -6,7 +6,6 @@
 use clap::{App, Arg};
 
 use std::process;
-// use std::str;
 
 use kms::operation::Encrypt;
 use kms::Blob;
@@ -44,20 +43,17 @@ async fn main() {
         .get_matches();
 
     let region = matches.value_of("region").unwrap_or("us-west-2");
-    let key = matches
-        .value_of("key")
-        .unwrap_or("0249d461-754b-4cbd-b874-372b294fa354");
-    let text = matches.value_of("text").unwrap_or("Encrypt this buckaroo");
+    let key = matches.value_of("key").unwrap_or("");
+    let text = matches.value_of("text").unwrap_or("");
 
     if region == "" || key == "" || text == "" {
-        println!("You must supply a value for region, key, and text (-r REGION -k KEY -t \"TEXT\"");
+        println!(
+            "You must supply a value for region, key, and text ([-r REGION] -k KEY -t \"TEXT\")"
+        );
+        println!("If REGION is not supplied, defaults to us-west-2");
 
         process::exit(1);
     }
-
-    println!("Region: {}", region);
-    println!("Key:    {}", key);
-    println!("Text:   {}", text);
 
     SubscriberBuilder::default()
         .with_env_filter("info")
@@ -82,10 +78,6 @@ async fn main() {
     // Did we get an encrypted blob?
     let blob = resp.ciphertext_blob.expect("Could not get encrypted text");
     let bytes = blob.as_ref();
-
-    // Wade through bytes, convert each one to a string, and add to string vector
-    //    let mut vBytes: Vec<u8> = Vec::new();
-
     let len = bytes.len();
     let mut i = 0;
 
@@ -93,51 +85,11 @@ async fn main() {
         if i < len - 1 {
             print!("{},", b);
         } else {
+            // Don't add a comma after the last item
             println!("{}", b);
         }
         i += 1;
     }
 
     println!("");
-    /*
-        for b in bytes {
-            let xs: [u8; 1] = [*b];
-            let s = str::from_utf8(&xs).unwrap();
-            println!("Char[{}]: {}", i, s);
-            i += 1;
-        }
-    */
-    //    let s = String::from_utf8_lossy(&bytes);
-
-    //    println!("String: {:?}", s);
-
-    /*
-    match resp.ciphertext_blob {
-        Some(x) => {
-            let bytes = x.as_ref();
-            println!("Bytes: {:?}", bytes);
-            println!("");
-
-            // println!("{}", std::str::from_utf8(&bytes).unwrap());
-
-            let s = String::from_utf8_lossy(&bytes);
-            println!("String: {:?}", s);
-            println!("");
-            println!("");
-
-            // Get an error:
-            //   invalid utf-8 sequence of 1 bytes from index 8', src\main.rs:94:27
-            // It looks like index 8's value is within a U8's range (0-255 if I'm not mistaken).
-            match str::from_utf8(&bytes) {
-                Ok(v) => println!("String: {}", v),
-                Err(e) => {
-                    // Barf out index 8
-                    println!("Eighth byte: {:?}", bytes[8]);
-                    panic!("Invalid UTF-8 sequence: {}", e)
-                }
-            };
-        }
-        None => println!("Could not encrypt string"),
-    }
-    */
 }
