0c08d79160bf65c8ccd705538214eb17ef829f1f
Doug Schwartz <dougsch@amazon.com>
2021-04-07 14:38:33 -0700

    Updated KMS code examples to mimic paths in smithy repo

--- ALPHA/kms/CreateKey/src/main.rs
+++ ALPHA/kms/CreateKey/src/main.rs
@@ -5,8 +5,10 @@
 
 use clap::{App, Arg};
 
-use kms::operation::CreateKey;
 use kms::Region;
+
+use std::process;
+
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::fmt::SubscriberBuilder;
 
@@ -21,32 +23,44 @@ async fn main() {
                 .help("Specifies the region")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .value_name("VERBOSE")
+                .help("Whether to display additional runtime information.")
+                .takes_value(false),
+        )
         .get_matches();
 
     let region = matches.value_of("region").unwrap_or("us-west-2");
+    let verbose = matches.is_present("verbose");
 
-    println!("Region: {}", region);
+    if verbose {
+        println!("Region: {}", region);
 
-    SubscriberBuilder::default()
-        .with_env_filter("info")
-        .with_span_events(FmtSpan::CLOSE)
-        .init();
-    let config = kms::Config::builder().region(Region::from(region)).build();
-
-    let client = aws_hyper::Client::https();
-
-    let data = client
-        .call(CreateKey::builder().build(&config))
-        .await
-        .expect("failed to create key");
-
-    println!("");
-
-    match data.key_metadata {
-        None => println!("No metadata found"),
-        Some(x) => match x.key_id {
-            None => println!("No key id"),
-            Some(k) => println!("{}", k),
-        },
+        SubscriberBuilder::default()
+            .with_env_filter("info")
+            .with_span_events(FmtSpan::CLOSE)
+            .init();
     }
+
+    // Create client
+    let config = kms::Config::builder().region(Region::from(region)).build();
+    let client = kms::Client::from_conf_conn(config, aws_hyper::conn::Standard::https());
+
+    match client.create_key().send().await {
+        Ok(data) => match data.key_metadata {
+            None => println!("No metadata found"),
+            Some(x) => match x.key_id {
+                None => println!("No key id"),
+                Some(k) => println!("\n\nKey:\n{}", k),
+            },
+        },
+        Err(_) => {
+            println!("");
+            process::exit(1);
+        }
+    };
+    println!("");
 }
