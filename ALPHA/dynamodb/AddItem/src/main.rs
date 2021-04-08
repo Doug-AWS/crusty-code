6b1fa7f4efbdcf5b14d88b9577881d372ad7d4a4
Doug Schwartz <dougsch@amazon.com>
2021-04-08 09:06:51 -0700

    Updated DynamoDB AddItem code example using latest Rust SDK bits

--- ALPHA/dynamodb/AddItem/src/main.rs
+++ ALPHA/dynamodb/AddItem/src/main.rs
@@ -3,120 +3,104 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use clap::{App, Arg};
 use std::collections::HashMap;
-use std::error::Error;
 use std::process;
 
 use dynamodb::model::AttributeValue;
-use dynamodb::operation::PutItem;
 use dynamodb::Region;
 
-use env_logger::Env;
+use structopt::StructOpt;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::SubscriberBuilder;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    /// The permission type of the user, standard_user or admin
+    #[structopt(short, long)]
+    p_type: String,
+
+    /// The user's age
+    #[structopt(short, long)]
+    age: String,
+
+    /// The user's username
+    #[structopt(short, long)]
+    username: String,
+
+    /// The user's first name
+    #[structopt(short, long)]
+    first: String,
+
+    /// The user's last name
+    #[structopt(short, long)]
+    last: String,
+
+    /// The table name
+    #[structopt(short, long)]
+    table: String,
+
+    /// The region
+    #[structopt(default_value = "us-west-2", short, long)]
+    region: String,
+
+    /// Activate verbose mode    
+    #[structopt(short, long)]
+    verbose: bool,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
+async fn main() {
+    let opt = Opt::from_args();
     // account_type, age, first_name, last_name
 
-    let matches = App::new("myapp")
-        .arg(
-            Arg::with_name("type")
-                .short("y")
-                .long("type")
-                .value_name("TYPE")
-                .help("The type of account: standard_user or admin")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("age")
-                .short("a")
-                .long("age")
-                .value_name("AGE")
-                .help("The age of the user")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("first")
-                .short("f")
-                .long("first")
-                .value_name("FIRST")
-                .help("The user's first name")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("last")
-                .short("l")
-                .long("last")
-                .value_name("LAST")
-                .help("The user's last name")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("region")
-                .short("r")
-                .long("region")
-                .value_name("REGION")
-                .help("Specifies the region to create the table in")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("table")
-                .short("t")
-                .long("table")
-                .value_name("TABLE")
-                .help("Specifies the table to create")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("username")
-                .short("u")
-                .long("username")
-                .value_name("USERNAME")
-                .help("The user name, which is the primary key of the table")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let region = matches.value_of("region").unwrap_or("us-west-2");
-
-    // TABLE AND KEY FOR TESTING ONLY. DELETE BEFORE RELEASING.
-    let table = matches.value_of("table").unwrap_or("users");
-    let username = matches.value_of("username").unwrap_or("");
-    let a_type = matches.value_of("type").unwrap_or("");
-    let age = matches.value_of("age").unwrap_or("");
-    let first = matches.value_of("first").unwrap_or("");
-    let last = matches.value_of("last").unwrap_or("");
-
-    if table == "" || username == "" || a_type == "" || age == "" || first == "" || last == "" {
-        println!("You must supply a table name, user name, type, age, and first and last names");
-        println!("-t TABLE -u USER-NAME -y TYPE -a AGE -f FIRST-NAME =l LAST-NAME)");
+    if opt.table == ""
+        || opt.username == ""
+        || opt.p_type == ""
+        || opt.age == ""
+        || opt.first == ""
+        || opt.last == ""
+    {
+        println!("\nYou must supply a table name, user name, permission type, age, and first and last names");
+        println!("-t TABLE -u USER-NAME -p PERMISSION-TYPE (admin or standard_user) -a AGE -f FIRST-NAME -l LAST-NAME)\n");
         process::exit(1);
     }
 
-    println!("Region: {}", region);
-    println!("Table:  {}", table);
-    println!("User:   {}", username);
-    println!("Type:   {}", a_type);
-    println!("Age:    {}", age);
-    println!("First:  {}", first);
-    println!("Last:   {}", last);
+    if opt.p_type != "standard_user" && opt.p_type != "admin" {
+        println!("\n{} is not a valid permission type", opt.p_type);
+        println!("You must specify a permission type value of 'admin' or 'standard_user':");
+        println!("-p PERMISSION-TYPE\n");
+        process::exit(1);
+    }
 
-    println!("DynamoDB client version: {}", dynamodb::PKG_VERSION);
+    if opt.verbose {
+        println!("DynamoDB client version: {}\n", dynamodb::PKG_VERSION);
+        println!("Region: {}", opt.region);
+        println!("Table:  {}", opt.table);
+        println!("User:   {}", opt.username);
+        println!("Type:   {}", opt.p_type);
+        println!("Age:    {}", opt.age);
+        println!("First:  {}", opt.first);
+        println!("Last:   {}\n", opt.last);
+
+        SubscriberBuilder::default()
+            .with_env_filter("info")
+            .with_span_events(FmtSpan::CLOSE)
+            .init();
+    }
+
     let config = dynamodb::Config::builder()
-        .region(Region::from(region))
+        .region(Region::new(opt.region))
         .build();
-    let client = aws_hyper::Client::https();
 
-    // Create hashmap(string, attributevalue)
-    // using "username",
+    let client = dynamodb::Client::from_conf_conn(config, aws_hyper::conn::Standard::https());
+
     let mut attrs: HashMap<String, AttributeValue> = HashMap::new();
-    let user_av = AttributeValue::S(username.to_string());
-    let type_av = AttributeValue::S(a_type.to_string());
-    let age_av = AttributeValue::S(age.to_string());
-    let first_av = AttributeValue::S(first.to_string());
-    let last_av = AttributeValue::S(last.to_string());
+    let user_av = AttributeValue::S(opt.username.to_string());
+    let type_av = AttributeValue::S(opt.p_type.to_string());
+    let age_av = AttributeValue::S(opt.age.to_string());
+    let first_av = AttributeValue::S(opt.first.to_string());
+    let last_av = AttributeValue::S(opt.last.to_string());
 
     attrs.insert("username".to_owned(), user_av);
     attrs.insert("account_type".to_owned(), type_av);
@@ -124,17 +108,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     attrs.insert("first_name".to_owned(), first_av);
     attrs.insert("last_name".to_owned(), last_av);
 
-    let op = PutItem::builder()
-        .table_name(table)
+    match client
+        .put_item()
+        .table_name(opt.table)
         .item(attrs)
-        .build(&config);
-
-    client.call(op).await?;
-
-    println!(
-        "Added user {}, {} {}, age {} as {} user",
-        username, first, last, age, a_type
-    );
-
-    Ok(())
+        .send()
+        .await
+    {
+        Ok(_) => println!(
+            "Added user {}, {} {}, age {} as {} user",
+            opt.username, opt.first, opt.last, opt.age, opt.p_type
+        ),
+        Err(e) => {
+            println!("Got an error adding item:");
+            println!("{:?}", e);
+            process::exit(1);
+        }
+    };
 }
