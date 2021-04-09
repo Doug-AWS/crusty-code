8b3fa82a7364962ae5a7e150bfa654ef4ac0bca4
Doug Schwartz <dougsch@amazon.com>
2021-04-09 12:36:03 -0700

    Updated DynamoDB DeleteItem code example in Rust

--- ALPHA/dynamodb/DeleteItem/src/main.rs
+++ ALPHA/dynamodb/DeleteItem/src/main.rs
@@ -3,93 +3,85 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use clap::{App, Arg};
-use std::collections::HashMap;
-use std::error::Error;
 use std::process;
 
 use dynamodb::model::AttributeValue;
-use dynamodb::operation::DeleteItem;
 use dynamodb::Region;
 
-use env_logger::Env;
+use structopt::StructOpt;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::SubscriberBuilder;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(default_value = "us-west-2", short, long)]
+    region: String,
+
+    /// The table name
+    #[structopt(short, long)]
+    table: String,
+
+    /// The key for the item in the table
+    #[structopt(short, long)]
+    key: String,
+
+    /// The value of the item to delete from the table
+    #[structopt(short, long)]
+    value: String,
+
+    /// Activate info mode
+    #[structopt(short, long)]
+    info: bool,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+async fn main() {
+    let opt = Opt::from_args();
 
-    let matches = App::new("myapp")
-        .arg(
-            Arg::with_name("region")
-                .short("r")
-                .long("region")
-                .value_name("REGION")
-                .help("Specifies the region to create the table in")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("table")
-                .short("t")
-                .long("table")
-                .value_name("TABLE")
-                .help("Specifies the table to create")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("key")
-                .short("k")
-                .long("key")
-                .value_name("KEY")
-                .help("The primary key of the table")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("value")
-                .short("v")
-                .long("value")
-                .value_name("VALUE")
-                .help("The value of the primary key to delete")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let region = matches.value_of("region").unwrap_or("us-west-2");
-
-    // TABLE AND KEY FOR TESTING ONLY. DELETE BEFORE RELEASING.
-    let table = matches.value_of("table").unwrap_or("users");
-    let key = matches.value_of("key").unwrap_or("");
-    let value = matches.value_of("value").unwrap_or("");
-
-    if table == "" || key == "" || value == "" {
-        println!("You must supply a table, key name, and key value");
-        println!("-t TABLE -k KEY -v VALUE)");
+    if opt.table == "" || opt.key == "" {
+        println!("\nYou must supply a table name and key");
+        println!("-t TABLE -k KEY)\n");
         process::exit(1);
     }
 
-    println!("Region: {}", region);
-    println!("Table:  {}", table);
-    println!("Key:    {}", key);
-    println!("VAlue:  {}", value);
+    if opt.info {
+        println!("DynamoDB client version: {}\n", dynamodb::PKG_VERSION);
+        println!("Region: {}", opt.region);
+        println!("Table:  {}", opt.table);
+        println!("Key:   {}\n", opt.key);
 
-    println!("DynamoDB client version: {}", dynamodb::PKG_VERSION);
+        SubscriberBuilder::default()
+            .with_env_filter("info")
+            .with_span_events(FmtSpan::CLOSE)
+            .init();
+    }
+
+    let t = &opt.table;
+    let k = &opt.key;
+    let v = &opt.value;
+    let r = &opt.region;
+
     let config = dynamodb::Config::builder()
-        .region(Region::from(region))
+        .region(Region::new(String::from(r)))
         .build();
-    let client = aws_hyper::Client::https();
 
-    let mut attrs: HashMap<String, AttributeValue> = HashMap::new();
-    let value_av = AttributeValue::S(value.to_string());
+    let client = dynamodb::Client::from_conf_conn(config, aws_hyper::conn::Standard::https());
 
-    attrs.insert(key.to_owned(), value_av);
-
-    let op = DeleteItem::builder()
-        .table_name(table)
-        .key(attrs)
-        .build(&config);
-
-    client.call(op).await?;
-
-    println!("Deleted key {} with value {}", key, value);
-
-    Ok(())
+    match client
+        .delete_item()
+        .table_name(String::from(t))
+        .key(String::from(k), AttributeValue::S(String::from(v)))
+        .send()
+        .await
+    {
+        Ok(_) => println!(
+            "Deleted {} from table {} in region {}",
+            opt.value, opt.table, opt.region
+        ),
+        Err(e) => {
+            println!("Got an error creating table:");
+            println!("{:?}", e);
+            process::exit(1);
+        }
+    };
 }
