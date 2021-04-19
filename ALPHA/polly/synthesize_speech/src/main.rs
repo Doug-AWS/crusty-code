e5b4b91625cb061de5061da1f903b2bfdfe234e5
Doug Schwartz <dougsch@amazon.com>
2021-04-19 10:28:09 -0700

    Updated code examples

--- ALPHA/polly/synthesize_speech/src/main.rs
+++ ALPHA/polly/synthesize_speech/src/main.rs
@@ -2,10 +2,15 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::process;
 
-use std::{env, process};
-
+use polly::model::{OutputFormat, VoiceId};
 use polly::{Client, Config, Region};
+
+use aws_types::region::{EnvironmentProvider, ProvideRegion};
 
 use structopt::StructOpt;
 use tracing_subscriber::fmt::format::FmtSpan;
@@ -13,34 +18,36 @@ use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(default_value = "", short, long)]
-    region: String,
+    /// The region
+    #[structopt(short, long)]
+    region: Option<String>,
 
+    /// The file containing the text to synthesize
+    #[structopt(short, long)]
+    filename: String,
+
+    /// Whether to show additional output
     #[structopt(short, long)]
     verbose: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut opt = Opt::from_args();
+    let Opt {
+        filename,
+        region,
+        verbose,
+    } = Opt::from_args();
 
-    let mut default_region = match env::var("AWS_DEFAULT_REGION") {
-        Ok(val) => val,
-        Err(_e) => String::from(""),
-    };
+    let region = EnvironmentProvider::new()
+        .region()
+        .or_else(|| region.as_ref().map(|region| Region::new(region.clone())))
+        .unwrap_or_else(|| Region::new("us-west-2"));
 
-    if default_region == "" {
-        default_region = String::from("us-west-2");
-    }
-
-    if opt.region == "" {
-        opt.region = default_region;
-    }
-
-    if opt.verbose {
+    if verbose {
         println!("polly client version: {}\n", polly::PKG_VERSION);
-        println!("Region: {}", opt.region);
-        // print any other opt settings
+        println!("Region:   {:?}", &region);
+        println!("Filename: {}", filename);
 
         SubscriberBuilder::default()
             .with_env_filter("info")
@@ -48,39 +55,43 @@ async fn main() {
             .init();
     }
 
-    let r = &opt.region;
+    //    let r = &opt.region;
+    let f = &filename;
 
-    let config = Config::builder()
-        .region(Region::new(String::from(r)))
-        .build();
+    let config = Config::builder().region(region).build();
 
     let client = Client::from_conf_conn(config, aws_hyper::conn::Standard::https());
 
-    match client.describe_voices().send().await {
-        Ok(resp) => {
-            println!("Voices in {}", opt.region);
-            let mut l = 0;
+    let content = fs::read_to_string(f);
 
-            for voice in resp.voices.iter() {
-                for v in voice.iter() {
-                    l += 1;
-                    match &v.name {
-                        None => {}
-                        Some(x) => println!("  Name:     {}", x),
-                    }
-                    match &v.language_name {
-                        None => {}
-                        Some(x) => println!("  Language: {}\n", x),
-                    }
-                }
-            }
-
-            println!("\nFound {} voices in {} region.\n", l, opt.region);
-        }
+    let resp = match client
+        .synthesize_speech()
+        .output_format(OutputFormat::Mp3)
+        .text(content.unwrap())
+        .voice_id(VoiceId::Joanna)
+        .send()
+        .await
+    {
+        Ok(output) => output,
         Err(e) => {
-            println!("Got an error describing voices:");
+            println!("Got an error synthesizing speech:");
             println!("{:?}", e);
             process::exit(1);
         }
     };
+
+    // Get MP3 code from response and save it
+    let blob = resp.audio_stream.expect("Could not get synthesized text");
+    let bytes = blob.as_ref();
+
+    // Create output filename from input filename
+
+    let parts: Vec<&str> = filename.split('.').collect();
+    let out_file = format!("{}{}", String::from(parts[0]), ".mp3");
+
+    let mut ofile = File::create(out_file).expect("unable to create file");
+
+    ofile
+        .write_all(bytes)
+        .expect("Could not write to output file");
 }
