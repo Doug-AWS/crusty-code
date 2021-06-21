7f53c39f45510d3670312562b77542dbe72bb704
Doug <dougsch@amazon.com>
2021-06-21 14:24:39 -0700

    WIP on SES code examples in Rust

--- ALPHA/ses/src/bin/send-message.rs
+++ ALPHA/ses/src/bin/send-message.rs
@@ -11,28 +11,38 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The name of the contact list.
-    #[structopt(short, long)]
-    contact_list: String,
-
     /// The AWS Region.
     #[structopt(short, long)]
     default_region: Option<String>,
 
-    /// The email address of the contact to add to the contact list.
+    /// The email address of the sender.
     #[structopt(short, long)]
-    email_address: String,
+    from_address: String,
+
+    /// The message of the email.
+    #[structopt(short, long)]
+    message: String,
+
+    /// The subject of the email.
+    #[structopt(short, long)]
+    subject: String,
+
+    /// The email address of the recepient.
+    #[structopt(short, long)]
+    to_address: String,
 
     /// Whether to display additional runtime information
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Adds a new contact to the contact list.
+/// Sends a message to the email address.
 /// # Arguments
 ///
-/// * `-c CONTACT-LIST` - The name of the contact list.
-/// * `-e EMAIL-ADDRESS` - The email address of the contact to add to the contact list.
+/// * `-f FROM-ADDRESS` - The email address of the sender.
+/// * `-m MESSAGE` - The email message that is sent.
+/// * `-s SUBJECT` - The subject of the email message.
+/// * `-t TO-ADDRESS` - The email address of the recepient.
 /// * `[-d DEFAULT-REGION]` - The region in which the client is created.
 ///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
@@ -40,9 +50,11 @@ struct Opt {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let Opt {
-        contact_list,
         default_region,
-        email_address,
+        from_address,
+        message,
+        subject,
+        to_address,
         verbose,
     } = Opt::from_args();
 
@@ -55,8 +67,10 @@ async fn main() -> Result<(), Error> {
     if verbose {
         println!("SES client version: {}", ses::PKG_VERSION);
         println!("Region:             {:?}", &region);
-        println!("Contact list:       {}", &contact_list);
-        println!("Email address:      {}", &email_address);
+        println!("From address:       {}", &from_address);
+        println!("To address:         {}", &to_address);
+        println!("Subject:            {}", &subject);
+        println!("Message:            {}", &message);
         println!();
     }
 
@@ -64,9 +78,8 @@ async fn main() -> Result<(), Error> {
     let client = Client::from_conf(conf);
 
     let new_contact = client
-        .create_contact()
-        .contact_list_name(contact_list)
-        .email_address(email_address)
+        .send_email()
+        .from_email_address(from_address)
         .send()
         .await;
     match new_contact {
