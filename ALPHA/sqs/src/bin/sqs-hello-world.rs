0b78d625978d0d1f9302e7f95ed4e26373e555f8
Doug <dougsch@amazon.com>
2021-06-30 13:22:25 -0700

    Updated SQS code examples to use common pattern; cleaned up Cargo.toml

--- ALPHA/sqs/src/bin/sqs-hello-world.rs
+++ ALPHA/sqs/src/bin/sqs-hello-world.rs
@@ -3,13 +3,55 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_types::region::ProvideRegion;
+use sqs::{Client, Config, Error, Region, PKG_VERSION};
 use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// The default AWS Region.
+    #[structopt(short, long)]
+    default_region: Option<String>,
+
+    /// Whether to display additional information.
+    #[structopt(short, long)]
+    verbose: bool,
+}
 
 /// Sends a message to and receives the message from a queue.
+/// /// # Arguments
+///
+/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
+///    If the environment variable is not set, defaults to **us-west-2**.
+/// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), sqs::Error> {
+async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
-    let client = sqs::Client::from_env();
+
+    let Opt {
+        default_region,
+        verbose,
+    } = Opt::from_args();
+
+    let region = default_region
+        .as_ref()
+        .map(|region| Region::new(region.clone()))
+        .or_else(|| aws_types::region::default_provider().region())
+        .unwrap_or_else(|| Region::new("us-west-2"));
+
+    println!();
+
+    if verbose {
+        println!("SQS version: {}", PKG_VERSION);
+        println!("Region:      {:?}", &default_region);
+        println!();
+    }
+
+    let config = Config::builder().region(region).build();
+    let client = Client::from_conf(config);
+
     let queues = client.list_queues().send().await?;
     let mut queue_urls = queues.queue_urls.unwrap_or_default();
     let queue_url = match queue_urls.pop() {
