09c1aff59c0336d3fb38bbb786cdd8414e9f2287
Doug <dougsch@amazon.com>
2021-06-30 06:40:24 -0700

    Updated Route53 code example to use common pattern; cleaned up Cargo.toml

--- ALPHA/route53/src/bin/route53-helloworld.rs
+++ ALPHA/route53/src/bin/route53-helloworld.rs
@@ -3,34 +3,30 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use route53::{Client, Config, Region};
-
 use aws_types::region::ProvideRegion;
-
+use route53::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region. Overrides environment variable AWS_DEFAULT_REGION.
+    /// The default AWS Region.
     #[structopt(short, long)]
     default_region: Option<String>,
 
-    /// Whether to display additional runtime information
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Displays the IDs and names of the hosted zones in the region.
+/// Displays the IDs and names of the hosted zones in the Region.
 /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), route53::Error> {
+async fn main() -> Result<(), Error> {
     let Opt {
         default_region,
         verbose,
@@ -42,14 +38,12 @@ async fn main() -> Result<(), route53::Error> {
         .or_else(|| aws_types::region::default_provider().region())
         .unwrap_or_else(|| Region::new("us-west-2"));
 
-    if verbose {
-        println!("Route53 client version: {}\n", route53::PKG_VERSION);
-        println!("Region:                 {:?}", &region);
+    println!();
 
-        SubscriberBuilder::default()
-            .with_env_filter("info")
-            .with_span_events(FmtSpan::CLOSE)
-            .init();
+    if verbose {
+        println!("Route53 version: {}", PKG_VERSION);
+        println!("Region:          {:?}", &region);
+        println!();
     }
 
     let conf = Config::builder().region(region).build();
@@ -57,7 +51,7 @@ async fn main() -> Result<(), route53::Error> {
     let hosted_zone_count = client.get_hosted_zone_count().send().await?;
 
     println!(
-        "\nNumber of hosted zones in region : {}",
+        "Number of hosted zones in region : {}",
         hosted_zone_count.hosted_zone_count.unwrap_or_default(),
     );
 
@@ -67,7 +61,9 @@ async fn main() -> Result<(), route53::Error> {
         let zone_name = hz.name.as_deref().unwrap_or_default();
         let zone_id = hz.id.as_deref().unwrap_or_default();
 
-        println!("Zone ID : {}, Zone Name : {}", zone_id, zone_name);
+        println!("  Zone ID :   {}", zone_id);
+        println!("  Zone Name : {}", zone_name);
+        println!();
     }
 
     Ok(())
