20b655405177f381655fe45c02e53cf0a56c79a3
Doug <dougsch@amazon.com>
2021-06-30 14:03:28 -0700

    Updated STS code examples to use common pattern; cleaned up Cargo.toml

--- ALPHA/sts/src/bin/credentials-provider.rs
+++ ALPHA/sts/src/bin/credentials-provider.rs
@@ -4,26 +4,71 @@
  */
 
 use aws_auth::{CredentialsError, ProvideCredentials};
+use aws_types::region::ProvideRegion;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, SystemTime};
+use structopt::StructOpt;
 use sts::Credentials;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// The default AWS Region.
+    #[structopt(short, long)]
+    default_region: Option<String>,
+
+    /// Whether to display additional information.
+    #[structopt(short, long)]
+    verbose: bool,
+}
 
 /// Implements a basic version of ProvideCredentials with AWS STS
 /// and lists the tables in the region based on those credentials.
+/// # Arguments
+///
+/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
+///    If the environment variable is not set, defaults to **us-west-2**.
+/// * `[-v]` - Whether to display additional information.
 #[tokio::main]
 async fn main() -> Result<(), dynamodb::Error> {
     tracing_subscriber::fmt::init();
-    let client = sts::Client::from_env();
+
+    let Opt {
+        default_region,
+        verbose,
+    } = Opt::from_args();
+
+    let region = default_region
+        .as_ref()
+        .map(|region| dynamodb::Region::new(region.clone()))
+        .or_else(|| aws_types::region::default_provider().region())
+        .unwrap_or_else(|| dynamodb::Region::new("us-west-2"));
+
+    println!();
+
+    if verbose {
+        println!("STS version:      {}", sts::PKG_VERSION);
+        println!("DynamoDB version: {}", dynamodb::PKG_VERSION);
+        println!("Region:           {:?}", &region);
+        println!();
+    }
+
+    let config = sts::Config::builder().region(region).build();
+    let client = sts::Client::from_conf(config);
+
     let sts_provider = StsCredentialsProvider {
         client,
         credentials: Arc::new(Mutex::new(None)),
     };
+
     sts_provider.spawn_refresh_loop().await;
 
     let dynamodb_conf = dynamodb::Config::builder()
         .credentials_provider(sts_provider)
         .build();
+
     let client = dynamodb::Client::from_conf(dynamodb_conf);
+
     println!("tables: {:?}", client.list_tables().send().await?);
     Ok(())
 }
