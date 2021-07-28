3d20009c311419f4c240872e16a318973f3ddf05
Doug <dougsch@amazon.com>
2021-07-28 06:30:23 -0700

    Updated batch code example to use latest bits

--- ALPHA/batch/src/bin/batch-helloworld.rs
+++ ALPHA/batch/src/bin/batch-helloworld.rs
@@ -1,12 +1,18 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+use aws_sdk_batch::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
 use aws_types::region::ProvideRegion;
-use batch::{Client, Config, Error, Region};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
     /// Whether to display additional information.
     #[structopt(short, long)]
@@ -16,38 +22,39 @@ struct Opt {
 /// Lists the names and the ARNs of your batch compute environments in a Region.
 /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///   If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///   If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display information.
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
-    let Opt {
-        default_region,
-        verbose,
-    } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let Opt { region, verbose } = Opt::from_args();
+
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
 
     println!();
 
     if verbose {
-        println!("Batch client version: {}", batch::PKG_VERSION);
-        println!("Region:               {:?}", &region);
+        println!("Batch client version: {}", PKG_VERSION);
+        println!(
+            "Region:               {}",
+            region.region().unwrap().as_ref()
+        );
         println!();
     }
 
-    let conf = Config::builder().region(&region).build();
+    let conf = Config::builder().region(region).build();
     let client = Client::from_conf(conf);
+
     let rsp = client.describe_compute_environments().send().await?;
 
     let compute_envs = rsp.compute_environments.unwrap_or_default();
     println!("Found {} compute environments:", compute_envs.len());
+
     for env in compute_envs {
         let arn = env.compute_environment_arn.as_deref().unwrap_or_default();
         let name = env.compute_environment_name.as_deref().unwrap_or_default();
