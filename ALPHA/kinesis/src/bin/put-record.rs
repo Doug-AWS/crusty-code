aaaa1df715dd66d495e417b16d6d53580f5c5f44
Doug <dougsch@amazon.com>
2021-09-08 09:56:08 -0700

    Updated Kinesis code examples to alpha 0.0.17 bits

--- ALPHA/kinesis/src/bin/put-record.rs
+++ ALPHA/kinesis/src/bin/put-record.rs
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use aws_types::region::ProvideRegion;
-use kinesis::{Client, Config, Error, Region, PKG_VERSION};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_kinesis::{Blob, Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -25,6 +25,7 @@ struct Opt {
     #[structopt(short, long)]
     stream_name: String,
 
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
@@ -51,29 +52,30 @@ async fn main() -> Result<(), Error> {
         verbose,
     } = Opt::from_args();
 
-    let region = region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
-
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
     println!();
 
     if verbose {
-        println!("Kinesis version: {}", PKG_VERSION);
-        println!("Region:          {:?}", &region);
+        println!("Kinesis client version: {}", PKG_VERSION);
+        println!(
+            "Region:                 {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
         println!("Data:");
         println!();
         println!("{}", &data);
         println!();
-        println!("Partition key:   {}", &key);
-        println!("Stream name:     {}", &stream_name);
+        println!("Partition key:          {}", &key);
+        println!("Stream name:            {}", &stream_name);
         println!();
     }
 
-    let config = Config::builder().region(region).build();
-    let client = Client::from_conf(config);
-    let blob = kinesis::Blob::new(data);
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    let blob = Blob::new(data);
 
     client
         .put_record()
