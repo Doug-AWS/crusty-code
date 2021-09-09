586b2c1a7f29b7062c30485f86652e8b35a4a300
Doug <dougsch@amazon.com>
2021-09-09 01:51:42 -0700

    Updated Qldb code examples to alpha 0.0.17 bits

--- ALPHA/qldb/src/bin/qldb-helloworld.rs
+++ ALPHA/qldb/src/bin/qldb-helloworld.rs
@@ -3,17 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use aws_types::region::ProvideRegion;
-use qldbsession::model::StartSessionRequest;
-use qldbsession::{Client, Config, Error, Region, PKG_VERSION};
-
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_qldbsession::model::StartSessionRequest;
+use aws_sdk_qldbsession::{Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
     /// The name of the ledger.
     #[structopt(short, long)]
@@ -24,11 +23,11 @@ struct Opt {
     verbose: bool,
 }
 
-/// Creates a low-level Amazon QLDB session.
+/// Creates a low-level Amazon Quantum Ledger Database (Amazon QLDB) session in the Region.
 /// # Arguments
 ///
 /// * `-l LEDGER` - The name of the ledger to start a new session against.
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
@@ -38,25 +37,28 @@ async fn main() -> Result<(), Error> {
 
     let Opt {
         ledger,
-        default_region,
+        region,
         verbose,
     } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
 
     if verbose {
-        println!("OLDB version: {}", PKG_VERSION);
-        println!("Region:       {:?}", &region);
-        println!("Ledger:       {}", ledger);
+        println!("OLDB client version: {}", PKG_VERSION);
+        println!(
+            "Region:              {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
+        println!("Ledger:              {}", ledger);
         println!();
     }
 
-    let conf = Config::builder().region(region).build();
-    let client = Client::from_conf(conf);
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
 
     let result = client
         .send_command()
