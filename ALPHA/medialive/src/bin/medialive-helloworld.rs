4746133b1f4c73ddd432a0842b26f350953676c3
Doug <dougsch@amazon.com>
2021-09-09 01:21:30 -0700

    Updated MediaLive code examples to alpha 0.0.17 bits

--- ALPHA/medialive/src/bin/medialive-helloworld.rs
+++ ALPHA/medialive/src/bin/medialive-helloworld.rs
@@ -3,15 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use aws_types::region::ProvideRegion;
-use medialive::{Client, Config, Error, Region, PKG_VERSION};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_medialive::{Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
     /// Whether to display additional information.
     #[structopt(short, long)]
@@ -21,7 +21,7 @@ struct Opt {
 /// Lists your AWS Elemental MediaLive input names and ARNs in the Region.
 /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
@@ -29,27 +29,24 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
-    let Opt {
-        default_region,
-        verbose,
-    } = Opt::from_args();
+    let Opt { region, verbose } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
-
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
     println!();
 
     if verbose {
-        println!("MediaLive version: {}", PKG_VERSION);
-        println!("Region:            {:?}", &region);
+        println!("MediaLive client version: {}", PKG_VERSION);
+        println!(
+            "Region:                   {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
         println!();
     }
 
-    let conf = Config::builder().region(region).build();
-    let client = Client::from_conf(conf);
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
 
     let input_list = client.list_inputs().send().await?;
 
@@ -57,7 +54,9 @@ async fn main() -> Result<(), Error> {
         let input_arn = i.arn.as_deref().unwrap_or_default();
         let input_name = i.name.as_deref().unwrap_or_default();
 
-        println!("Input Name : {}, Input ARN : {}", input_name, input_arn);
+        println!("Input Name : {}", input_name);
+        println!("Input ARN : {}", input_arn);
+        println!();
     }
 
     Ok(())
