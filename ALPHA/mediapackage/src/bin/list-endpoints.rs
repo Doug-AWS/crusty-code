8f332ac1d3974140e2396451d697553feb5e1081
Doug <dougsch@amazon.com>
2021-09-09 01:29:07 -0700

    Updated MediaPackage code examples to alpha 0.0.17 bits

--- ALPHA/mediapackage/src/bin/list-endpoints.rs
+++ ALPHA/mediapackage/src/bin/list-endpoints.rs
@@ -3,26 +3,25 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-/// Lists your AWS Elemental MediaPackage endpoint URLs.
-use aws_types::region::ProvideRegion;
-use mediapackage::{Client, Config, Error, Region, PKG_VERSION};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_mediapackage::{Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
     /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Lists your AWS Elemental MediaPackage endpoint descriptions and URLs.
+/// Lists your AWS Elemental MediaPackage endpoint descriptions and URLs in the Region.
 /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
@@ -30,37 +29,35 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
-    let Opt {
-        default_region,
-        verbose,
-    } = Opt::from_args();
+    let Opt { region, verbose } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
-
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
     println!();
 
     if verbose {
-        println!("MediaPackage version: {}", PKG_VERSION);
-        println!("Region:               {:?}", &region);
+        println!("MediaPackage client version: {}", PKG_VERSION);
+        println!(
+            "Region:                      {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
         println!();
     }
 
-    let conf = Config::builder().region(region).build();
-    let client = Client::from_conf(conf);
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
 
     let or_endpoints = client.list_origin_endpoints().send().await?;
+
+    println!("Endpoints:");
 
     for e in or_endpoints.origin_endpoints.unwrap_or_default() {
         let endpoint_url = e.url.as_deref().unwrap_or_default();
         let endpoint_description = e.description.as_deref().unwrap_or_default();
-        println!(
-            "Endpoint Description: {}, Endpoint URL : {}",
-            endpoint_description, endpoint_url
-        );
+        println!("  Description: {}", endpoint_description);
+        println!("  URL :        {}", endpoint_url);
+        println!();
     }
 
     Ok(())
